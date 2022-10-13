3fadeb8f748021308a46e5912bc8e238fb4097ed
kkmaurice <adonyomaurice@gmail.com>
2022-10-14 00:38:46 +0300

    Added main class to keep track of the course class

--- composition.dart
+++ composition.dart
@@ -72,15 +72,4 @@ class Course {
   }
 }
 
-void main() {
-  var course = Course(
-      name: 'Dart Programming',
-      book: Book(name: 'Introduction to programming', isbn: '123-22-34255'),
-      instructor: Instructor(name: 'John Rambo', gender: 'M', age: 45));
 
-  // print(course.book.name);
-   course.book.name = 'Introduction to C';
-  // print(course.book.name);
-  print(course);
-  print(course.book.isbn);
-}
